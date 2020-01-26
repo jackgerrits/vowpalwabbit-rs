b18805678cd33f591bf88953d943353fb9f35bfd
Jack Gerrits <jackgerrits95@gmail.com>
2020-01-26 17:30:34 -0500

    change boost version

--- prepare_submodules.sh
+++ prepare_submodules.sh
@@ -7,7 +7,5 @@ git submodule update --init external/boost
 git submodule update --init external/boost-cmake
 git submodule update --init external/zlib
 cd external/boost
-git submodule update --init libs/program_options
-git submodule update --init libs/thread
-git submodule update --init libs/system
+git submodule update --init libs/*
 cd ../../../../
