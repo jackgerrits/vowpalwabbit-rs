aa6103fa8019213c5f45f7bcfc5e858dcdd17453
Jack Gerrits <jackgerrits95@gmail.com>
2020-01-26 18:02:45 -0500

    Update deps

--- prepare_submodules.sh
+++ prepare_submodules.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+shopt -s extglob
 git submodule update --init external/vowpal_wabbit
 cd external/vowpal_wabbit
 git submodule update --init rapidjson
 git submodule update --init external/boost
 git submodule update --init external/boost-cmake
 git submodule update --init external/zlib
-cd external/boost
-git submodule update --init libs/*
+cd external/boost/libs
+rm -rf !(config|detail|range|iterator|headers|function|lexical_cast|any|type_traits|static_cast|core|program_options|math)
+git submodule update --init *
 cd ../../../../
