b66b018c2374aecfcdf9dabc962b0eaa0b813465
Jack Gerrits <jackgerrits95@gmail.com>
2020-01-26 17:12:28 -0500

    Update dependency mechanism to submodules

---
+++ prepare_submodules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git submodule update --init external/vowpal_wabbit
+cd external/vowpal_wabbit
+git submodule update --init rapidjson
+git submodule update --init external/boost
+git submodule update --init external/boost-cmake
+git submodule update --init external/zlib
+cd external/boost
+git submodule update --init libs/program_options
+git submodule update --init libs/thread
+git submodule update --init libs/system
+cd ../../../../
